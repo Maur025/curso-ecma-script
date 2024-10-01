86cfe64776deb6f5d22748d011996007d62379d9
maur025 <mmoyapalli@gmail.com>
2024-10-01 00:14:06 -0400

    lesson 05 fix

--- src/es6/05-object-literals.js
+++ src/es6/05-object-literals.js
@@ -1,13 +1,5 @@
 // enhaced object literals
 
-function newUser(user, age, country) {
-  return {
-    user: user,
-    age: age,
-    country: country,
-  };
-}
-
 function newUser(user, age, country, uId) {
   return {
     user,
